78a37408a462913a2341fd451091fa5b71ff4191
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-09 22:56:44 +0300

    Create Game component

---
+++ src/components/Game.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "0";
+
+  return (
+    <>
+      <h1>React Tic Tac Toe</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
